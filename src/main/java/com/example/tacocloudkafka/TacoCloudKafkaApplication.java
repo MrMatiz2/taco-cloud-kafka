ed2d80bd820926f2f94b90f65ed4f335c56a3558
MrMatiz2 <alejandromatiz87@gmail.com>
2024-12-02 20:03:15 -0500

    Initial commit

---
+++ src/main/java/com/example/tacocloudkafka/TacoCloudKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.example.tacocloudkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCloudKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TacoCloudKafkaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tacocloudkafka/TacoCloudKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tacocloudkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudKafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
