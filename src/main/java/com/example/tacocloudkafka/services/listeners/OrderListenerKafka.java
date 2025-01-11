aa370468509a495fdb7e42f176d37ac445addd0f
MrMatiz2 <alejandromatiz87@gmail.com>
2025-01-10 21:35:31 -0500

    Deploying and building Spring refactor

--- src/main/java/com/example/tacocloudkafka/services/listeners/OrderListenerKafka.java
+++ src/main/java/com/example/tacocloudkafka/services/listeners/OrderListenerKafka.java
@@ -1,6 +1,7 @@
 package com.example.tacocloudkafka.services.listeners;
 
 import com.example.tacoclouddomain.entities.TacoOrder;
+import com.example.tacoclouddomain.kitchen.KitchenOrder;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
@@ -9,15 +10,15 @@ import org.springframework.stereotype.Component;
 @Component
 public class OrderListenerKafka {
 
-    private final KitchenListenerKafka kitchenListenerKafka;
+    private final KitchenOrder kitchenOrder;
 
-    public OrderListenerKafka(KitchenListenerKafka kitchenListenerKafka) {
-        this.kitchenListenerKafka = kitchenListenerKafka;
+    public OrderListenerKafka(KitchenOrder kitchenOrder) {
+        this.kitchenOrder = kitchenOrder;
     }
 
     @KafkaListener(topics = "tacocloud.orders.topic")
     public void receiveOrder(@Payload TacoOrder order) {
-        kitchenListenerKafka.displayOrder(order);
+        kitchenOrder.displayOrder(order);
     }
 
 }
